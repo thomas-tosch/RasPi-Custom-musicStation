b53695fdd7fe8cdb8d46c33f7fec9701477c21b3
thomas.tosch <thomas.tosch@uha.fr>
2019-04-14 21:05:36 +0200

    Added methods in preparation of the incoming player class

--- main.py
+++ main.py
@@ -13,22 +13,40 @@ def play_vlc(audio_url, queue):
     media = instance.media_new(audio_url.url)
     player.set_media(media)
     player.play()
-    x = 0
     while player.get_length() < 1:
         time.sleep(0.05)
     time.sleep(player.get_length() / 1000)
     queue.remove()
 
 
+def add_song(name, list):
+    ytb = Youtube()
+    url = ytb.get_one(name)
+    pafy = ytb.get_audio(url)
+    list.add(pafy)
+
+
+def play_after_this_one(name, list):
+    ytb = Youtube()
+    url = ytb.get_one(name)
+    pafy = ytb.get_audio(url)
+    list.add(pafy)
+
+
 def main():
     t = None
-    queue = List()
-    ytb = Youtube()
-    url = ytb.get_one('thunderstruck')
-    pafy = ytb.get_audio(url)
-    queue.add(pafy)
-    t = threading.Thread(target=play_vlc, args=(queue.queue[0], queue))
-    t.start()
+    list = List()
+    add_song('thunderstruck', list)
+    add_song('redbone', list)
+    while True:
+        if list.queue is None or not list.queue:
+            break
+        if t is None:
+            t = threading.Thread(target=play_vlc, args=(list.queue[0], list))
+            t.start()
+        if not t.is_alive():
+            t = threading.Thread(target=play_vlc, args=(list.queue[0], list))
+            t.start()
 
 
 if __name__ == '__main__':
